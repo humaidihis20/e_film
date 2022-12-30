aeb165ae8eee7f89327f00daa26c2daa411e3bc6
humaidihis20 <60593297+humaidihis20@users.noreply.github.com>
2022-12-30 10:00:10 +0700

    DB migration and seeder

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Movie;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $movie = [
+            [
+                'name' => 'The Shawshank Redemption',
+                'slug' => 'the-shawshank-redemption',
+                'category' => 'Drama',
+                'video_url' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
+                'thumbnail' => 'https://cdn.pixabay.com/photo/2018/03/27/18/52/football-3266982_1280.jpg',
+                'rating' => 9.3,
+                'is_featured' => 1
+            ],
+
+            [
+                'name' => 'The Goodfather',
+                'slug' => 'the-goodfather',
+                'category' => 'Drama',
+                'video_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+                'thumbnail' => 'https://cdn.pixabay.com/photo/2018/06/20/22/59/train-3487575_1280.jpg',
+                'rating' => 9.2,
+                'is_featured' => 0
+            ],
+
+            [
+                'name' => 'The Goodfather: Part II',
+                'slug' => 'the-goodfather-part-ii',
+                'category' => 'Drama',
+                'video_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+                'thumbnail' => 'https://cdn.pixabay.com/photo/2018/03/27/18/51/mohr-heads-3266981_1280.jpg',
+                'rating' => 9.1,
+                'is_featured' => 0
+            ],
+        ];
+
+        Movie::insert($movie);
+    }
+}
